1dcd2236c96aef7ff14e74fdd0e5ca453ad0b907
Bal1oon <ws32159@naver.com>
2024-12-05 12:57:45 +0900

    feat: 사원 삭제 구현

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,8 +1,12 @@
 package app.api.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.entity.Emp;
 import app.repository.EmpRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -11,5 +15,12 @@ public class EmpAPIController {
  
 	private final EmpRepository empRepository;
 	
+	@DeleteMapping("/api/emp/{empno}")
+	public Emp deleteEmpByEmpno(@PathVariable Integer empno) {
+		Emp emp = empRepository.findById(empno)
+				.orElseThrow(() -> new EntityNotFoundException("Not Found the Employee"));
+		empRepository.delete(emp);
+		return emp;
+	}
 	
 }
