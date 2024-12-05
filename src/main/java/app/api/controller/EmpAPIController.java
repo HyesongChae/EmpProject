57c16efeb4eadc71395a5a9028d14bad73ba09da
KangDaeHyun <jodae00@hanmail.net>
2024-12-05 13:42:04 +0900

    feat/emps-get-all_emp-get-empno

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,7 +1,13 @@
 package app.api.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.entity.Emp;
 import app.repository.EmpRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +17,18 @@ public class EmpAPIController {
  
 	private final EmpRepository empRepository;
 	
+	@GetMapping("/emps")
+	public List<Emp> getAllEmployees() {
+	    return empRepository.findAll();
+	}
+
+	@GetMapping("/emp/{empno}")
+	public ResponseEntity<Emp> getEmployeeById(@PathVariable int empno) {
+	    return empRepository.findById(empno)
+	            .map(ResponseEntity::ok)
+	            .orElse(ResponseEntity.notFound().build());
+	}
+
+
 	
 }
